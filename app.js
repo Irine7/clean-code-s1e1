a2f308680e01c82578aad2be1da6f277ec558f5f
Irine7 <irine7@yandex.ru>
2023-04-23 14:58:34 +0200

    refactor(basic-3.6): space after named properties

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
 
-var taskInput=document.getElementById("new-task");
-var addButton=document.getElementsByTagName("button")[0];
-var incompleteTaskHolder=document.getElementById("incompleteTasks");
-var completedTasksHolder=document.getElementById("completed-tasks");
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTaskHolder = document.getElementById("incompleteTasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
 
 
-var createNewTaskElement=function(taskString){
-  var listItem=document.createElement("li");
-  var checkBox=document.createElement("input");
-  var label=document.createElement("label");
-  var editInput=document.createElement("input");
-  var editButton=document.createElement("button");
-  var deleteButton=document.createElement("button");
-  var deleteButtonImg=document.createElement("img");
+var createNewTaskElement = function(taskString){
+  var listItem = document.createElement("li");
+  var checkBox = document.createElement("input");
+  var label = document.createElement("label");
+  var editInput = document.createElement("input");
+  var editButton = document.createElement("button");
+  var deleteButton = document.createElement("button");
+  var deleteButtonImg = document.createElement("img");
 
-  label.innerText=taskString;
-  label.className="task";
+  label.innerText = taskString;
+  label.className = "task";
 
-  checkBox.type="checkbox";
-  editInput.type="text";
-  editInput.className="task";
-  editButton.innerText="Edit";
-  editButton.className="edit";
-  deleteButton.className="delete";
-  deleteButtonImg.src="./remove.svg";
+  checkBox.type = "checkbox";
+  editInput.type = "text";
+  editInput.className = "task";
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.className = "delete";
+  deleteButtonImg.src = "remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
   listItem.appendChild(checkBox);
@@ -36,83 +36,83 @@ var createNewTaskElement=function(taskString){
   return listItem;
 }
 
-var addTask=function(){
+var addTask = function() {
   console.log("Add Task...");
   if (!taskInput.value) return;
   var listItem=createNewTaskElement(taskInput.value);
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
-  taskInput.value="";
+  taskInput.value = "";
 }
 
-var editTask=function(){
+var editTask = function() {
   console.log("Edit Task...");
   console.log("Change 'edit' to 'save'");
 
-  var listItem=this.parentNode;
-  var editInput=listItem.querySelector("input[type=text]");
-  var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("edit-mode");
-  if(containsClass){
+  var listItem = this.parentNode;
+  var editInput = listItem.querySelector("input[type=text]");
+  var label = listItem.querySelector("label");
+  var editBtn = listItem.querySelector(".edit");
+  var containsClass = listItem.classList.contains("edit-mode");
 
+  if (containsClass){
     label.innerText=editInput.value;
     editBtn.innerText="Edit";
-  }else{
-    editInput.value=label.innerText;
-    editBtn.innerText="Save";
+  } else {
+    editInput.value = label.innerText;
+    editBtn.innerText = "Save";
 }
 
   listItem.classList.toggle("edit-mode");
 };
 
-var deleteTask=function(){
+var deleteTask = function(){
 
   console.log("Delete Task...");
-  var listItem=this.parentNode;
-  var ul=listItem.parentNode;
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
   ul.removeChild(listItem);
 }
 
-var taskCompleted=function(){
+var taskCompleted = function(){
 
   console.log("Complete Task...");
-  var listItem=this.parentNode;
+  var listItem = this.parentNode;
   completedTasksHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskIncomplete);
 }
 
-var taskIncomplete=function(){
+var taskIncomplete = function(){
   console.log("Incomplete Task...");
-  var listItem=this.parentNode;
+  var listItem = this.parentNode;
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem,taskCompleted);
 }
 
-var ajaxRequest=function(){
+var ajaxRequest = function(){
   console.log("AJAX Request");
 }
 
-addButton.onclick=addTask;
+addButton.onclick = addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+var bindTaskEvents = function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
 
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
 
-  editButton.onclick=editTask;
-  deleteButton.onclick=deleteTask;
-  checkBox.onchange=checkBoxEventHandler;
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
+  checkBox.onchange = checkBoxEventHandler;
 }
 
-for (var i=0; i<incompleteTaskHolder.children.length;i++){
+for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
   bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
 }
 
-for (var i=0; i<completedTasksHolder.children.length;i++){
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
   bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 }
